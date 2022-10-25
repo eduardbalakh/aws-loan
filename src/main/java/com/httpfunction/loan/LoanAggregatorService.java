ef833d9cf6af5e4e8db812439769bc8db4e5d302
EBALAKH <Eduard_Balakh@epam.com>
2022-10-25 22:10:14 +0600

    add aggregation

--- src/main/java/com/httpfunction/loan/LoanAggregatorService.java
+++ src/main/java/com/httpfunction/loan/LoanAggregatorService.java
@@ -1,6 +1,11 @@
 package com.httpfunction.loan;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class LoanAggregatorService {
@@ -15,17 +20,54 @@ public class LoanAggregatorService {
         loanDataProvider.saveLoan(loanDO);
     }
 
-    public List<LoanDO> getLoansForBorrower(String borrowerId) {
+    public List<LoanDO> getAllLoansForBorrower(String borrowerId) {
         return loanDataProvider.getLoansByBorrowerId(borrowerId);
     }
 
-    public List<LoanDO> getLoansForOwner(String ownerId) {
+    public List<LoanDO> getAllLoansForOwner(String ownerId) {
         return loanDataProvider.getLoansByOwnerId(ownerId);
     }
 
-    public List<LoanDO> getLoansForOwnerAndBorrower(String ownerId, String borrowerId) {
-        return getLoansForOwner(ownerId).stream()
+    public List<LoanDO> getAllLoansForOwnerAndBorrower(String ownerId, String borrowerId) {
+        return getAllLoansForOwner(ownerId).stream()
                 .filter(loan -> loan.getBorowerId().equals(borrowerId))
                 .collect(Collectors.toList());
     }
+
+    public List<AggregatedLoanDO> getAggregatedLoanForBorrower(String borrowerId) {
+        return getAggregatedLoanList(loanDataProvider.getLoansByBorrowerId(borrowerId));
+    }
+
+    public List<AggregatedLoanDO> getAggregatedLoadForOwner(String ownerId) {
+        return getAggregatedLoanList(loanDataProvider.getLoansByOwnerId(ownerId));
+    }
+
+    private List<AggregatedLoanDO> getAggregatedLoanList(List<LoanDO> listOfLoans) {
+        Map<String, AggregatedLoanDO> map = new HashMap<>();
+        listOfLoans.forEach(loanDO -> {
+            if (map.containsKey(loanDO.getLoanOwnerId())) {
+                AggregatedLoanDO oldAggregatedLoanDO = map.get(loanDO.getLoanOwnerId());
+                AggregatedLoanDO newAggregatedLoanDO = mergeTwoAggregatedLoans(oldAggregatedLoanDO, loanDO);
+                map.put(loanDO.getLoanOwnerId(), newAggregatedLoanDO);
+            } else {
+                map.put(loanDO.getLoanOwnerId(), new AggregatedLoanDO(loanDO.getLoanOwnerId(), loanDO.getBorowerId(), loanDO.getValue(), loanDO.getDateTime(), loanDO.getDateTime()));
+            }
+        });
+        return new ArrayList<>(map.values());
+    }
+
+    private AggregatedLoanDO mergeTwoAggregatedLoans(AggregatedLoanDO oldAggregate, LoanDO newLoan) {
+        AggregatedLoanDO newAggregatedLoanDO = new AggregatedLoanDO();
+        newAggregatedLoanDO.setOwnerId(oldAggregate.getOwnerId());
+        newAggregatedLoanDO.setValue(oldAggregate.getValue().add(newLoan.getValue()));
+        newAggregatedLoanDO.setBorrowerId(oldAggregate.getBorrowerId());
+        if (oldAggregate.getStartDate().isAfter(newLoan.getDateTime())) {
+            newAggregatedLoanDO.setStartDate(newLoan.getDateTime());
+        }
+        if (oldAggregate.getEndDate().isBefore(newLoan.getDateTime())) {
+            newAggregatedLoanDO.setEndDate(newLoan.getDateTime());
+        }
+        return newAggregatedLoanDO;
+    }
+
 }
